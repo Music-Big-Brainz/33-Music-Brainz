8b299ba83fb78bfa9f4eb8b0eed967e37bfbc76f
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-11 17:39:10 -0800

    artist snapshot test passing

--- src/components/albums/AlbumList.test.jsx
+++ src/components/albums/AlbumList.test.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import AlbumList from './AlbumList';
+import MemoryRouter from 'react-router-dom';
 
 describe('displays a list of albums', () => {
   afterEach(() => cleanup());
   it('renders a list of albums', () => {
-    const { asFragment } = render(<AlbumList title="" date="" />);
+    const { asFragment } = render(
+      <MemoryRouter>
+        <AlbumList title="" date="" />
+      </MemoryRouter>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
