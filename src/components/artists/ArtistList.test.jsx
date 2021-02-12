839b2838e134ca27a5a46af84885136201d9ac94
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-12 15:48:51 -0800

    four tests passing

--- src/components/artists/ArtistList.test.jsx
+++ src/components/artists/ArtistList.test.jsx
@@ -3,12 +3,12 @@ import { render, cleanup } from '@testing-library/react';
 import Artist from './Artist';
 import { MemoryRouter } from 'react-router-dom';
 
-describe('displays an Artist', () => {
+describe('displays artists', () => {
   afterEach(() => cleanup());
-  it('renders one single artist', () => {
+  it('renders artists', () => {
     const { asFragment } = render(
       <MemoryRouter>
-        <Artist artistId="" name="" disambiguation />
+        <Artist artists="" />
       </MemoryRouter>
     );
     expect(asFragment()).toMatchSnapshot();
