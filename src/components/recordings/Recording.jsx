0bd196bac0d2b9afa448ebecf5c7dc0ccac8f006
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-12 18:02:32 -0800

    api update

--- src/components/recordings/Recording.jsx
+++ src/components/recordings/Recording.jsx
@@ -3,22 +3,24 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 
-const Recording = ({ recordingId, songTitle }) => {
+const RecordingList = ({ recordings, name, albumId }) => {
+  const { title } = useParams();
+  const recordingElements = recordings.map(recording => (
+    <li key={recording.id}>
+      <Recording {...recording} name={name} albumId={albumId} />
+    </li>
+  ));
+
   return (
-    <Link to={`${recordingId}`}>
-      <figure>
-const Recording = ({ recordingId, songTitle, name, albumId }) => {
-  return (
-    <Link to={`/releases/${name}/title/${albumId}/${songTitle}/${songTitle}`}>
-      <figure key={recordingId}>
-        <p>{songTitle}</p>
-      </figure>
-    </Link>
+    <>
+      <h1>{title}</h1>
+      <ul>{recordingElements}</ul>;
+    </>
   );
 };
 
-Recording.propTypes =
-=======
+Recording.propTypes = {
+
   name: PropTypes.string.isRequired,
   albumId: PropTypes.string.isRequired,
   recordingId: PropTypes.string.isRequired,
