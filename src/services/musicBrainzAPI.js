1dfc8d7a990646ff74456dbeb3feeeae4965f01c
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-14 15:27:04 -0800

    hotfix for App.jsx

--- src/services/musicBrainzAPI.js
+++ src/services/musicBrainzAPI.js
@@ -1,6 +1,6 @@
 export const findArtistByName = (search) => {
   return fetch(
-    `http://musicbrainz.org/ws/2/artist?query=${search}&fmt=json&limit=25`
+    `https://musicbrainz.org/ws/2/artist?query=${search}&fmt=json&limit=25`
   )
     .then((res) => res.json())
     .then(({ artists }) =>
